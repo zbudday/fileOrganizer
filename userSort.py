fad2e015e6e79968403a088bdcd5138d652c00c8
Zachary Budday <zbudday@gmail.com>
2026-03-04 10:54:50 -0500

    Updated userSort.py to handle subfolders and delete empty folders

--- userSort.py
+++ userSort.py
@@ -4,30 +4,37 @@ import shutil
 #C:/Users/Zachary Budday/Desktop/messy
 #C:/Users/Zachary Budday/Desktop/messy2
 
-FOLDER_PATH = input("Print the full path you want to serach: ")
+FOLDER_PATH = input("Print the full path you want to search: ")
+if not os.path.exists(FOLDER_PATH):
+    print("The path you typed in does not exist... exiting")
+    quit()
+            
+
+def buffer_call():
+    input("Press Enter to continue... ")
 
 
 def print_folders():
-    for folder in os.listdir(FOLDER_PATH):
-        full_path = os.path.join(FOLDER_PATH, folder)
-        if os.path.isdir(full_path): #checks to see if it is a directory 
-            print(folder)
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for folder in dirs:
+            full_path = os.path.join(root, folder)
+            if os.path.isdir(full_path): #checks to see if it is a directory 
+                print(folder)
 
 
 def print_files():
-    for file in os.listdir(FOLDER_PATH):
-        full_path = os.path.join(FOLDER_PATH, file)
-        if os.path.isfile(full_path): #checks to see if it is a file
-            print(file)
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for file in files:
+            full_path = os.path.join(root, file)
+            if os.path.isfile(full_path): #checks to see if it is a file
+                print(file)
 
 
 def move_files():
-    for file in os.listdir(FOLDER_PATH):
-        full_file_path = os.path.join(FOLDER_PATH, file)
+    for root, dirs, files in os.walk(FOLDER_PATH):
+        for file in files:
+            full_file_path = os.path.join(root, file)
         
-        
-        #loops through only files and asks the user if they want to move them
-        if os.path.isfile(full_file_path):
             user_input = input(f"Do you want to move {full_file_path}: \n1.Yes \n2.No")
 
             #if user wants to move it
@@ -51,18 +58,38 @@ def move_files():
                 print("not a valid option")
         
 
+def del_empty():
+    for root, dirs, files in os.walk(FOLDER_PATH, topdown=False):
+        if len(dirs) == 0 and len(files) == 0:
+            c1 = input(f"{root} is empty, would you like to delete it? \n1.Yes \n2.No")
+            if c1 == "1":
+                os.rmdir(root)
+            elif c1 == "2":
+                continue
+            else:
+                print("Not a valid choice")
+            buffer_call()
+            
+            
+        
 
 def menu():
     while True:
-        choice = input(f"Do you want to \n1. View the files within your folder\n2. View all subfolders in your folder\n3. Sort your files \n4. Quit: ")
+        choice = input(f"Do you want to \n1. View the files within your folder\n2. View all subfolders in your folder\n3. Sort your files \n4. Delete empty folders \n5. Quit: ")
     
         if choice == "1":
             print_files()
+            buffer_call()
         elif choice == "2":
             print_folders()
+            buffer_call()
         elif choice == "3":
             move_files()
+            buffer_call()
         elif choice == "4":
+            del_empty()
+        elif choice == "5":
             break
+
 
 menu()
